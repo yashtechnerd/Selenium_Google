65f0d9b26a6f625cab8517f04f0423f3072268dd
yashtechnerd <153451122+yashtechnerd@users.noreply.github.com>
2024-01-09 11:51:58 +0530

    Create Googlesearch.java

---
+++ src/test/java/com/qascript/Googlesearch.java
@@ -0,0 +1,34 @@
+package google_search;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+public class Googlesearch{
+   @SuppressWarnings("deprecation")
+
+  
+public static void main(String[]Args ) {
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\yashwanth.s\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+      WebDriver driver = new ChromeDriver();
+      driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+      driver.get("https://www.google.com/");
+      // identify element
+      WebElement p=driver.findElement(By.name("q"));
+      //enter text with sendKeys() then apply submit()
+      p.sendKeys("Selenium Java");
+      // Explicit wait condition for search results
+      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+      wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul")));
+      p.submit();
+      //driver.close();
+   }
+}
